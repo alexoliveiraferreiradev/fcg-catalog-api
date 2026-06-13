c098454f3709925273788022d98f753ac567084b
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-13 12:28:22 -0300

    refactor: move DesativarPromocaoInvalidaCommandHandler para a pasta DesativarPromocaoInvalida

--- src/Fcg.Catalogo.Application/Features/Jogos/Commands/DesativarPromocao/DesativarPromocaoInvalidaCommandHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Fcg.Catalogo.Application.Features.Jogos.Commands.DesativarPromocao
-{
-    public class DesativarPromocaoInvalidaCommandHandler
-    {
-    }
-}
---
+++ src/Fcg.Catalogo.Application/Features/Jogos/Commands/DesativarPromocaoInvalida/DesativarPromocaoInvalidaCommandHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Fcg.Catalogo.Application.Features.Jogos.Commands.DesativarPromocaoInvalida
+{
+    public class DesativarPromocaoInvalidaCommandHandler
+    {
+    }
+}
