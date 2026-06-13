0e3a3be8dfe6e3317eda5460796b86d38576f9a8
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-13 11:29:25 -0300

    refactor: adiciona assinaturas do IBibliotecaService

--- src/Fcg.Catalogo.Application/Interfaces/IBibliotecaService.cs
+++ src/Fcg.Catalogo.Application/Interfaces/IBibliotecaService.cs
@@ -1,6 +1,13 @@
-﻿namespace Fcg.Catalogo.Application.Interfaces
+﻿using Fcg.Catalogo.Application.Common;
+using Fcg.Catalogo.Application.Dtos.Biblioteca;
+
+namespace Fcg.Catalogo.Application.Interfaces
 {
     public interface IBibliotecaService
     {
+        Task LiberarJogosAposPedido(Guid usuarioId, List<Guid> jogosIds);
+        Task<PagedResult<BibliotecaResponse>> ObtemBibliotecaDoUsuarioPaginacao(Guid usuarioId, int pagina = 1, int tamanhoPagina = 10);
+        Task<bool> VerificaSeUsuarioPossuiJogo(Guid usuarioId, Guid jogoId);
+        Task<IEnumerable<Guid>> ObterIdsJogosDoUsuario(Guid usuarioId);
     }
 }
