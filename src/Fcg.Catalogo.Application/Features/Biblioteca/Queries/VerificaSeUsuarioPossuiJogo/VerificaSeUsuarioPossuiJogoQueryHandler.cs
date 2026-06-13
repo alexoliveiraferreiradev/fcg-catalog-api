3352fbb29add875bb5b5064e8dbaf9e10fd40014
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-13 11:41:42 -0300

    feat: add VerificaSeUsuarioPossuiJogoQueryHandler ao projeto

---
+++ src/Fcg.Catalogo.Application/Features/Biblioteca/Queries/VerificaSeUsuarioPossuiJogo/VerificaSeUsuarioPossuiJogoQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fcg.Catalogo.Application.Features.Biblioteca.Queries.VerificaSeUsuarioPossuiJogo
+{
+    internal class VerificaSeUsuarioPossuiJogoQueryHandler
+    {
+    }
+}
