af35bd4ac80ddb2228c89466e9326705ab25ecd0
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-13 12:19:02 -0300

    refactor: renomeia pasta AtualizarJogoCommand para AtualizarJogo

---
+++ src/Fcg.Catalogo.Application/Features/Jogos/Commands/AtualizarJogo/AtualizarJogoCommandHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Fcg.Catalogo.Application.Features.Jogos.Commands.AtualizarJogo
+{
+    public class AtualizarJogoCommandHandler
+    {
+    }
+}
--- src/Fcg.Catalogo.Application/Features/Jogos/Commands/AtualizarJogoCommand/AtualizarJogoCommandHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Fcg.Catalogo.Application.Features.Jogos.Commands.AtualizarJogoCommand
-{
-    public class AtualizarJogoCommandHandler
-    {
-    }
-}
