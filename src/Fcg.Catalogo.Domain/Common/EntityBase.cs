bbe8362a552a5d47786de8307ee1dea9bf5888cb
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 12:28:48 -0300

    feat: adiciona classe base EntityBase

---
+++ src/Fcg.Catalogo.Domain/Common/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fcg.Catalogo.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public Guid Id { get; private set; }
+        public EntityBase()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        protected abstract void ValidarEntidade();
+    }
+}
