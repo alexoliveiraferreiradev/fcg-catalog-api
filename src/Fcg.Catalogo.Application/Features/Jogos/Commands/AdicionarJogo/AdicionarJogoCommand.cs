98df766dcd184e523236ea5f947666b1aa3357fd
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 11:58:23 -0300

    refactor: adiciona propriedades em AdicionarJogoCommand

--- src/Fcg.Catalogo.Application/Features/Jogos/Commands/AdicionarJogo/AdicionarJogoCommand.cs
+++ src/Fcg.Catalogo.Application/Features/Jogos/Commands/AdicionarJogo/AdicionarJogoCommand.cs
@@ -1,6 +1,24 @@
-﻿namespace Fcg.Catalogo.Application.Features.Jogos.Commands.AdicionarJogo
+﻿using Fcg.Catalogo.Application.Dtos.Jogos;
+using Fcg.Catalogo.Domain.Enum;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fcg.Catalogo.Application.Features.Jogos.Commands.AdicionarJogo
 {
-    public class AdicionarJogoCommand
+    public class AdicionarJogoCommand : IRequest<JogosResponse>
     {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public GeneroJogo Genero { get; set; }
+
+        public AdicionarJogoCommand()
+        {
+        }
+
+        public AdicionarJogoCommand(string nomeJogo, string descricaoJogo, decimal precoJogo, GeneroJogo generoJogo)
+        {
+            Nome = nomeJogo; Descricao = descricaoJogo; Preco = precoJogo; Genero = generoJogo;
+        }
     }
 }
