8d5f4e377d06379c591b9c7b82f324fb634c60a3
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 11:58:26 -0300

    feat: adiciona DTO JogosResponse

---
+++ src/Fcg.Catalogo.Application/Dtos/Jogos/JogosResponse.cs
@@ -0,0 +1,25 @@
+﻿using Fcg.Catalogo.Domain.Enum;
+
+namespace Fcg.Catalogo.Application.Dtos.Jogos
+{
+    public class JogosResponse
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal PrecoOriginal { get; set; }
+        public decimal PrecoAtual { get; set; }
+        public GeneroJogo Genero { get; set; }
+        public bool Ativo { get; set; }
+        public bool TemDesconto => PrecoAtual < PrecoOriginal;
+
+        public JogosResponse()
+        {
+        }
+
+        public JogosResponse(Guid jogoId, string nomeJogo, string descricaoJogo, decimal precoOriginal, decimal precoAtual, GeneroJogo generoJogo)
+        {
+            Id = jogoId; Nome = nomeJogo; Descricao = descricaoJogo; PrecoOriginal = precoOriginal; PrecoAtual = precoAtual; Genero = generoJogo;
+        }
+    }
+}
