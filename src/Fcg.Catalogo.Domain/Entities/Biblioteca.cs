2ba2fd319d0af0318bc0c48a300ef270e729046e
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 12:28:49 -0300

    feat: adiciona entidade Biblioteca

---
+++ src/Fcg.Catalogo.Domain/Entities/Biblioteca.cs
@@ -0,0 +1,32 @@
+﻿using Fcg.Catalogo.Domain.Common;
+using Fcg.Catalogo.Domain.Resources;
+
+namespace Fcg.Catalogo.Domain.Entities
+{
+    public class Biblioteca : AggregateRoot
+    {       
+        public Guid UsuarioId { get; private set; }
+        public virtual Jogo Jogo { get; private set; }
+        public Guid JogoId { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public DateTime DataAlteracao { get; private set; }
+        public bool Ativo { get; private set; }
+
+        public Biblioteca(Guid usuarioId, Guid jogoId)
+        {
+            UsuarioId = usuarioId;
+            JogoId = jogoId;
+            DataCadastro = DateTime.UtcNow;
+            DataAlteracao = DataCadastro;
+            Ativo = true;
+            ValidarEntidade();
+        }
+
+        protected override void ValidarEntidade()
+        {
+            AssertionConcern.AssertArgumentNotEquals(UsuarioId, Guid.Empty, MensagensDominio.UsuarioNaoEncontrado);
+            AssertionConcern.AssertArgumentNotEquals(JogoId, Guid.Empty, MensagensDominio.JogoNaoEncontrado);
+        }
+
+    }
+}
