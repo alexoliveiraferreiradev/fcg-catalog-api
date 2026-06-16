114cdd62a2b47990882359b04bab84ca813562bb
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 11:58:23 -0300

    refactor: adiciona propriedades em ObtemBibliotecaPaginadaQuery

--- src/Fcg.Catalogo.Application/Features/Biblioteca/Queries/ObtemBibliotecaPaginada/ObtemBibliotecaPaginadaQuery.cs
+++ src/Fcg.Catalogo.Application/Features/Biblioteca/Queries/ObtemBibliotecaPaginada/ObtemBibliotecaPaginadaQuery.cs
@@ -2,5 +2,19 @@
 {
     public class ObtemBibliotecaPaginadaQuery
     {
+        public Guid JogoId { get; set; }
+        public string NomeJogo { get; set; }
+        public string Descricao { get; set; }
+        public string Genero { get; set; }
+        public DateTime DataAquisicao { get; set; }
+
+        public ObtemBibliotecaPaginadaQuery()
+        {
+        }
+
+        public ObtemBibliotecaPaginadaQuery(Guid jogoid, string nomeJogo, string descricaojogo, string generoJogo, DateTime dataAquisicao)
+        {
+            JogoId = jogoid; NomeJogo = nomeJogo; Descricao = descricaojogo; Genero = generoJogo; DataAquisicao = dataAquisicao;
+        }
     }
 }
