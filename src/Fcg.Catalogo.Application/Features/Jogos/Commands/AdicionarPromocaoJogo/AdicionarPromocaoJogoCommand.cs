dcba3023a05db719901e1185acf19c30a84befd6
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 11:58:24 -0300

    refactor: adiciona propriedades em AdicionarPromocaoJogoCommand

--- src/Fcg.Catalogo.Application/Features/Jogos/Commands/AdicionarPromocaoJogo/AdicionarPromocaoJogoCommand.cs
+++ src/Fcg.Catalogo.Application/Features/Jogos/Commands/AdicionarPromocaoJogo/AdicionarPromocaoJogoCommand.cs
@@ -1,6 +1,22 @@
-﻿namespace Fcg.Catalogo.Application.Features.Jogos.Commands.AdicionarPromocaoJogo
+﻿using Fcg.Catalogo.Application.Dtos.Promocao;
+using MediatR;
+
+namespace Fcg.Catalogo.Application.Features.Jogos.Commands.AdicionarPromocaoJogo
 {
-    public class AdicionarPromocaoJogoCommand
+    public class AdicionarPromocaoJogoCommand : IRequest<PromocaoResponse>
     {
+        public Guid JogoId { get; set; }
+        public decimal ValorPromocao { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+
+        public AdicionarPromocaoJogoCommand()
+        {
+        }
+
+        public AdicionarPromocaoJogoCommand(Guid jogoid, decimal valorPromocao, DateTime dataInicio, DateTime dataFim)
+        {
+            JogoId = jogoid; ValorPromocao = valorPromocao; DataInicio = dataInicio; DataFim = dataFim;
+        }
     }
 }
