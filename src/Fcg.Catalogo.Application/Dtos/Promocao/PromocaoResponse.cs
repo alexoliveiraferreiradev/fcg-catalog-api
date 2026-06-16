484cf8c5e1de7b6326b680f30d262c10130441d6
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 11:58:27 -0300

    feat: adiciona DTO PromocaoResponse

---
+++ src/Fcg.Catalogo.Application/Dtos/Promocao/PromocaoResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Fcg.Catalogo.Application.Dtos.Promocao
+{
+    public class PromocaoResponse
+    {
+        public Guid PromocaoId { get; set; }
+        public Guid JogoId { get; set; }
+        public decimal ValorPromocao { get; set; }
+        public string NomeJogo { get; set; }
+        public string DescricaoJogo { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+    }
+}
