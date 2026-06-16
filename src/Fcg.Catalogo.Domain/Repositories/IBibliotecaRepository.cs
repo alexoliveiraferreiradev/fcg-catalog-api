6cf8fd55f4f83ae37c373c021eea6a94bffed725
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 17:07:12 -0300

    refactor(domain): remove metodos Atualizar e ObterPorId de IBibliotecaRepository

--- src/Fcg.Catalogo.Domain/Repositories/IBibliotecaRepository.cs
+++ src/Fcg.Catalogo.Domain/Repositories/IBibliotecaRepository.cs
@@ -5,8 +5,6 @@ namespace Fcg.Catalogo.Domain.Repositories
     public interface IBibliotecaRepository
     {
         Task Adicionar(Biblioteca biblioteca);
-        Task Atualizar(Biblioteca biblioteca);
-        Task<Biblioteca> ObterPorId(Guid id);
         Task<bool> VerificaSeUsuarioPossuiJogo(Guid usuarioId, Guid jogoId);
         Task SaveChanges();
     }
