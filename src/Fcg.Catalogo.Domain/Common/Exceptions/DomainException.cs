900f1a69efe0c3ba894e5e8dd39534463472c145
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 12:28:49 -0300

    feat: adiciona excecao customizada DomainException

---
+++ src/Fcg.Catalogo.Domain/Common/Exceptions/DomainException.cs
@@ -0,0 +1,26 @@
+﻿namespace Fcg.Catalogo.Domain.Common.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException()
+        {
+
+        }
+        /// <summary>
+        /// Cria instância de DomainException com mensagem personalizada
+        /// </summary>
+        /// <param name="message"></param>
+        public DomainException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Cria instância de DomainException com mensagem personalizada e nome do parâmetro relacionado
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="paramName"></param>
+        public DomainException(string message, string paramName) : base($"{message} (Parâmetro: {paramName})")
+        {
+        }
+    }
+}
