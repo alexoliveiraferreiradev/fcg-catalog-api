99a4fe92bab6e3de20dea29ec49b88523176c324
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 11:53:53 -0300

    refact: convert queries to records in Features/Jogos/Queries

--- src/Fcg.Catalogo.Application/Features/Jogos/Queries/ObtemJogosPaginados/ObtemJogosPaginadosQuery.cs
+++ src/Fcg.Catalogo.Application/Features/Jogos/Queries/ObtemJogosPaginados/ObtemJogosPaginadosQuery.cs
@@ -1,6 +1,14 @@
-﻿namespace Fcg.Catalogo.Application.Features.Jogos.Queries.ObtemJogosPaginados
+using Fcg.Catalogo.Application.Common;
+using Fcg.Catalogo.Application.Dtos.Jogos;
+using Fcg.Catalogo.Domain.Enum;
+using MediatR;
+
+namespace Fcg.Catalogo.Application.Features.Jogos.Queries.ObtemJogosPaginados
 {
-    public class ObtemJogosPaginadosQuery
-    {
-    }
+    public record ObtemJogosPaginadosQuery(
+        int Pagina = 1,
+        int Tamanho = 10,
+        string? Busca = null,
+        GeneroJogo? Genero = null,
+        bool? ApenasPromovidos = null) : IRequest<PagedResult<JogosResponse>>;
 }
