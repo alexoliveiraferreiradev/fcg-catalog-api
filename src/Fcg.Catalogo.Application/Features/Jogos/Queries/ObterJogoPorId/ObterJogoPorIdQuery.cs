75b8c7f0bfc9f801d3b1db4b2ea56e3259212ce8
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 11:58:26 -0300

    refactor: adiciona propriedades em ObterJogoPorIdQuery

--- src/Fcg.Catalogo.Application/Features/Jogos/Queries/ObterJogoPorId/ObterJogoPorIdQuery.cs
+++ src/Fcg.Catalogo.Application/Features/Jogos/Queries/ObterJogoPorId/ObterJogoPorIdQuery.cs
@@ -1,6 +1,7 @@
-﻿namespace Fcg.Catalogo.Application.Features.Jogos.Queries.ObterJogoPorId
+﻿using Fcg.Catalogo.Application.Dtos.Jogos;
+using MediatR;
+
+namespace Fcg.Catalogo.Application.Features.Jogos.Queries.ObterJogoPorId
 {
-    public class ObterJogoPorIdQuery
-    {
-    }
+    public record ObterJogoPorIdQuery(Guid jogoId) : IRequest<JogosResponse>;
 }
