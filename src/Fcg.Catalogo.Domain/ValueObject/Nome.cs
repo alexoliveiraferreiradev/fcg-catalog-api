a963ea8c2a284e551ceea80b4996cd9d46171e2e
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 12:28:51 -0300

    feat: adiciona objeto de valor Nome

---
+++ src/Fcg.Catalogo.Domain/ValueObject/Nome.cs
@@ -0,0 +1,28 @@
+﻿using Fcg.Catalogo.Domain.Common;
+using Fcg.Catalogo.Domain.Resources;
+
+namespace Fcg.Catalogo.Domain.ValueObject
+{
+    public class Nome : ValueObject<Nome>
+    {
+        public string Valor { get; }
+
+        public Nome(string valor)
+        {
+            AssertionConcern.AssertArgumentRealValues(valor, MensagensDominio.NomeJogoNaoReal);
+            AssertionConcern.AssertArgumentEmpty(valor, MensagensDominio.JogoNomeObrigatorio);
+            AssertionConcern.AssertArgumentLength(valor, 3, 100, MensagensDominio.JogoTamanhoNomeInvalido);
+            Valor = valor;
+        }
+
+        protected override bool EqualsCore(Nome other)
+        {
+            return Valor == other.Valor;
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return Valor.GetHashCode();
+        }
+    }
+}
