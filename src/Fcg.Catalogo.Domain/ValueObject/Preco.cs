649cfa7f25f1252ced74c0b22a038925c8ce6fc3
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-16 12:28:51 -0300

    feat: adiciona objeto de valor Preco

---
+++ src/Fcg.Catalogo.Domain/ValueObject/Preco.cs
@@ -0,0 +1,31 @@
+﻿using Fcg.Catalogo.Domain.Common;
+using Fcg.Catalogo.Domain.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fcg.Catalogo.Domain.ValueObject
+{
+    public class Preco : ValueObject<Preco>
+    {
+        public decimal Valor { get; }
+
+        public Preco(decimal valor)
+        {
+            AssertionConcern.AssertArgumentValueFormat(valor, MensagensDominio.ValorInvalido);
+            Valor = valor;
+        }
+
+        protected override bool EqualsCore(Preco other)
+        {
+            return Valor == other.Valor;
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return Valor.GetHashCode();
+        }
+    }
+}
