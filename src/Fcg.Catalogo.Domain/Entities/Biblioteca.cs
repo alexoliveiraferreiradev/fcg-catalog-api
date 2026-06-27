f147a047f2bfcfa1abd74cdb09fdfc69e44e89a3
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-27 19:29:59 -0300

    refactor(domain): remover consumer obsoleto de UserCreatedEvent e construtor inconsistente de biblioteca

--- src/Fcg.Catalogo.Domain/Entities/Biblioteca.cs
+++ src/Fcg.Catalogo.Domain/Entities/Biblioteca.cs
@@ -1,4 +1,4 @@
-﻿using Fcg.Catalogo.Domain.Resources;
+using Fcg.Catalogo.Domain.Resources;
 using Fcg.Core.Abstractions.Common;
 
 namespace Fcg.Catalogo.Domain.Entities
@@ -16,15 +16,6 @@ namespace Fcg.Catalogo.Domain.Entities
         {
             UsuarioId = usuarioId;
             JogoId = jogoId;
-            DataCadastro = DateTime.UtcNow;
-            DataAlteracao = DataCadastro;
-            Ativo = true;
-            ValidarEntidade();
-        }
-
-        public Biblioteca(Guid usuarioId)
-        {
-            UsuarioId = usuarioId;
             DataCadastro = DateTime.UtcNow;
             DataAlteracao = DataCadastro;
             Ativo = true;
