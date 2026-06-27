31c1ae9aa8e15c6bd3ff4349cf1d02bd0b1ebede
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-27 16:01:33 -0300

    feat(queries): add ObtemTodosJogosQuery and handler structure

---
+++ src/Fcg.Catalogo.Application/Features/Catalogo/Queries/ObtemTodosJogos/ObtemTodosJogosQuery.cs
@@ -0,0 +1,7 @@
+﻿using Fcg.Catalogo.Application.Features.Response;
+using MediatR;
+
+namespace Fcg.Catalogo.Application.Features.Catalogo.Queries.ObtemTodosJogos
+{
+    public record ObtemTodosJogosQuery() : IRequest<IEnumerable<JogosResponse>>;
+}
---
+++ src/Fcg.Catalogo.Application/Features/Catalogo/Queries/ObtemTodosJogos/ObtemTodosJogosQueryHandler.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Fcg.Catalogo.Application.Features.Response;
+using MediatR;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fcg.Catalogo.Application.Features.Catalogo.Queries.ObtemTodosJogos
+{
+    public class ObtemTodosJogosQueryHandler : IRequestHandler<ObtemTodosJogosQuery, IEnumerable<JogosResponse>>
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public ObtemTodosJogosQueryHandler(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task<IEnumerable<JogosResponse>> Handle(ObtemTodosJogosQuery request, CancellationToken cancellationToken)
+        {
+            const string sql = @"
+            SELECT 
+                j.Id, 
+                j.Nome, 
+                j.Descricao, 
+                j.PrecoBase as PrecoOriginal,             
+                    COALESCE(
+                        (SELECT TOP 1 p.ValorPromocao 
+                        FROM Promocoes p 
+                        WHERE p.JogoId = j.Id 
+                        AND GETUTCDATE() BETWEEN p.DataInicio AND p.DataFim), 
+                        j.PrecoBase
+                    ) as PrecoAtual,
+                j.Ativo,
+                j.Genero
+            FROM Jogos j ";
+            var jogos = await _dbConnection.QueryAsync<JogosResponse>(sql);
+           
+            return jogos;
+        }
+    }
+}
