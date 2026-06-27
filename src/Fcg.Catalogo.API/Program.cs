d55a3d818851540cfab813109b576e9f2dde8456
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-27 17:05:44 -0300

    feat(api): configurar Swagger, injetar repositorios e mapear endpoints minimal API

--- src/Fcg.Catalogo.API/Program.cs
+++ src/Fcg.Catalogo.API/Program.cs
@@ -1,16 +1,33 @@
+using Fcg.Catalogo.API.Endpoints.Admin;
+using Fcg.Catalogo.API.Endpoints.Anonymous;
+using Fcg.Catalogo.API.Endpoints.Usuario;
 using Fcg.Catalogo.Application.Features.Catalogo.Commands.AdicionarJogo;
+using Fcg.Catalogo.Domain.Repositories;
 using Fcg.Catalogo.Infrastructure.Persistence;
+using Fcg.Catalogo.Infrastructure.Repository;
+using Fcg.Core.Abstractions.Interfaces;
 using Fcg.Core.WebApi.Security;
+using FluentValidation;
 using MassTransit;
 using Microsoft.EntityFrameworkCore;
-using FluentValidation;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "FIAP Cloud Games API",
+        Version = "v1",
+        Description = "API para gestão de catálogo de jogos e processamento de pedidos.",
+        Contact = new OpenApiContact
+        {
+            Name = "Alex Oliveira Ferreira"
+        }
+    });
+});
 builder.Services.AddOpenApi();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<CatalogoDbContext>(options =>
@@ -41,19 +58,32 @@ builder.Services.AddValidatorsFromAssembly(typeof(AdicionarJogoCommand).Assembly
 
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
 
+builder.Services.AddScoped<CatalogoDbContext>();
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+builder.Services.AddScoped<IJogoRepository, JogoRepository>();  
+builder.Services.AddScoped<IBibliotecaRepository, BibliotecaRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapGerenciaJogoEndpoint();
+app.MapGerenciaPromocaoEndpoint();
+app.MapCatalogoJogosEndpoint();
+app.MapBibliotecaUsuarioPaginadaEndpoint();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fiap Cloud Games");
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
 
 app.Run();
