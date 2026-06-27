399e0f16d2feab0159e268c1aaece7c5319e6a32
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-27 16:27:16 -0300

    refactor(commands): converter DesativarJogoCommand para record posicional

--- src/Fcg.Catalogo.Application/Features/Catalogo/Commands/DesativarJogo/DesativarJogoCommand.cs
+++ src/Fcg.Catalogo.Application/Features/Catalogo/Commands/DesativarJogo/DesativarJogoCommand.cs
@@ -2,8 +2,5 @@ using MediatR;
 
 namespace Fcg.Catalogo.Application.Features.Catalogo.Commands.DesativarJogo
 {
-    public record DesativarJogoCommand : IRequest
-    {
-        public Guid JogoId { get; init; }
-    }
+    public record DesativarJogoCommand(Guid jogoId) : IRequest;
 }
