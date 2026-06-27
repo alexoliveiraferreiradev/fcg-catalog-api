2863d415da731beb7703c9ceb8ace06bea88129d
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-27 14:52:46 -0300

    feat(api): implement minimal API endpoints for catalog

---
+++ src/Fcg.Catalogo.API/Endpoints/Anonymous/CatalogoJogosEndpoint.cs
@@ -0,0 +1,80 @@
+﻿using Fcg.Catalogo.Application.Features.Catalogo.Queries.ObtemCatalogoPaginados;
+using Fcg.Catalogo.Domain.Enum;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fcg.Catalogo.API.Endpoints.Anonymous
+{
+    public static class CatalogoJogosEndpoint
+    {
+        public static void MapCatalogoJogosEndpoint(this WebApplication app)
+        {
+
+            var group = app.MapGroup("/api/catalogo-jogos").WithTags("Catálogo de Jogos").AllowAnonymous();
+
+            group.MapGet("/obtem-todos",
+                async ([FromServices] ISender mediator,
+                       [FromQuery] int pagina = 1,
+                       [FromQuery] int tamanho = 10) =>
+                {
+                    var query = new ObtemCatalogoPaginadosQuery(pagina, tamanho);
+
+                    var response = await mediator.Send(query);
+                    if (response == null)
+                    {
+                        return Results.NotFound();
+                    }
+                    return Results.Ok(response);
+                })
+                .Produces(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound);
+
+
+            group.MapGet("obtem-por-genero/{genero}",
+                async ([FromServices] ISender mediator,
+                       [FromRoute] GeneroJogo genero,
+                       [FromQuery] int pagina = 1,
+                       [FromQuery] int tamanho = 10) =>
+                {
+                    var query = new ObtemCatalogoPaginadosQuery() with
+                    {
+                        Pagina = pagina,
+                        TamanhoPagina = tamanho,
+                        Genero = genero
+                    };
+
+                    var response = await mediator.Send(query);
+                    if (response == null)
+                    {
+                        return Results.NotFound();
+                    }
+                    return Results.Ok(response);
+                })
+                .Produces(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound);
+
+
+            group.MapGet("obtem-promovidos",
+                async ([FromServices] ISender mediator,
+                       [FromQuery] int pagina = 1,
+                       [FromQuery] int tamanho = 10) =>
+                {
+                    var query = new ObtemCatalogoPaginadosQuery() with
+                    {
+                        Pagina = pagina,
+                        TamanhoPagina = tamanho,
+                        ApenasPromovidos = true
+                    };
+                    var response = await mediator.Send(query);
+                    if (response == null)
+                    {
+                        return Results.NotFound();
+                    }
+                    return Results.Ok(response);
+                })
+                .Produces(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound);
+
+        }
+    }
+}
