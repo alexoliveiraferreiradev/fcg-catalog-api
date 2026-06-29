ab9e5314d059d78e25db9925d3ec06593131ddbe
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-29 00:25:42 -0300

    feat(application/api): implementar funcionalidade e endpoint de AdquirirJogo

---
+++ src/Fcg.Catalogo.API/Endpoints/Usuario/AdquirirJogoEndpoint.cs
@@ -0,0 +1,20 @@
+﻿namespace Fcg.Catalogo.API.Endpoints.Usuario
+{
+    public static class AdquirirJogoEndpoint
+    {
+
+
+        //private static async Task<IResult> AdquirirJogo(
+        //    [FromServices] ISender sender,
+        //    [FromBody] AdquirirJogoCommand command,
+        //    CancellationToken cancellationToken)
+        //{
+        //    var response = await sender.Send(command, cancellationToken);
+        //    if (!response.Sucesso)
+        //    {
+        //        return Results.BadRequest(response.Mensagens);
+        //    }
+        //    return Results.Ok(response.Mensagens);
+        //}
+    }
+}
---
+++ src/Fcg.Catalogo.Application/Features/Catalogo/Commands/AcessoGeral/AdquirirJogo/AdquirirJogoCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Fcg.Catalogo.Application.Features.Catalogo.Commands.AcessoGeral.AdquirirJogo
+{
+    public record AdquirirJogoCommand(Guid UsuarioId,IEnumerable<Guid> JogosIds) : IRequest<bool>;
+}
