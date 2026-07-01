976222c1216acb2cfdd6067988d7768e1a576ab8
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-01 00:41:18 -0300

    feat(infrastructure): implementar servico de cache utilizando Redis

---
+++ src/Fcg.Catalogo.Infrastructure/Caching/RedisOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Fcg.Catalogo.Infrastructure.Caching
+{
+    public class RedisOptions
+    {
+        public string Configuration { get; set; }
+        public string InstanceName { get; set; }
+    }
+}
