204dbe2078f8d17372d75734cbde5f7b1fd9392a
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-01 00:16:01 -0300

    feat(api): adicionar extensoes para mapeamento de endpoints, seguranca e seeding de dados

---
+++ src/Fcg.Catalogo.API/Extensions/SecurityExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Fcg.Catalogo.API.Extensions
+{
+    public static class SecurityExtensions
+    {
+        public static WebApplication UseSecurityMiddlewares(this WebApplication app)
+        {
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            return app;
+        }
+    }
+}
