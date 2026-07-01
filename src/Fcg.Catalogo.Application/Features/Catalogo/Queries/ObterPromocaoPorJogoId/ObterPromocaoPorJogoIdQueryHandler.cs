ddbc1ca1754663dd3cc7dfe61ec4b19e8c353d5b
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-01 07:20:23 -0300

    feat(application): implementar cache na consulta de promocao por id do jogo

--- src/Fcg.Catalogo.Application/Features/Catalogo/Queries/ObterPromocaoPorJogoId/ObterPromocaoPorJogoIdQueryHandler.cs
+++ src/Fcg.Catalogo.Application/Features/Catalogo/Queries/ObterPromocaoPorJogoId/ObterPromocaoPorJogoIdQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using Fcg.Catalogo.Application.Common.Interfaces;
 using Fcg.Catalogo.Application.Features.Response;
 using MediatR;
 using System.Data;
@@ -8,14 +9,27 @@ namespace Fcg.Catalogo.Application.Features.Catalogo.Queries.ObterPromocaoPorJog
     public class ObterPromocaoPorJogoIdQueryHandler : IRequestHandler<ObterPromocaoPorJogoIdQuery, PromocaoResponse>
     {
         private readonly IDbConnection _dbConnection;
+        private readonly ICacheService _cacheService;
 
-        public ObterPromocaoPorJogoIdQueryHandler(IDbConnection dbConnection)
+        public ObterPromocaoPorJogoIdQueryHandler(IDbConnection dbConnection,
+            ICacheService cacheService)
         {
             _dbConnection = dbConnection;
+            _cacheService = cacheService;
         }
 
         public async Task<PromocaoResponse> Handle(ObterPromocaoPorJogoIdQuery request, CancellationToken cancellationToken)
         {
+            var cacheKey = $"catalogo:promocao:detalhes:{request.promocaoId}";
+
+            var cachedPromocao = await _cacheService.GetAsync<PromocaoResponse>(cacheKey, cancellationToken);
+
+            if (cachedPromocao != null)
+            {
+                return cachedPromocao;
+            }
+
+
             const string sql = @"
             SELECT 
                 p.Id AS PromocaoId,
@@ -28,7 +42,16 @@ namespace Fcg.Catalogo.Application.Features.Catalogo.Queries.ObterPromocaoPorJog
             FROM Promocoes p
             INNER JOIN Jogos j ON p.JogoId = j.Id
             WHERE p.Id = @PromocaoId;";
-            return await _dbConnection.QueryFirstOrDefaultAsync<PromocaoResponse>(sql, new { PromocaoId = request.promocaoId });
+
+            var promocaoDetalhe = await _dbConnection.QueryFirstOrDefaultAsync<PromocaoResponse>(sql, new { PromocaoId = request.promocaoId });
+
+            if(promocaoDetalhe != null)
+            {
+                await _cacheService.SetAsync(cacheKey, promocaoDetalhe, TimeSpan.FromMinutes(5), cancellationToken);
+            }
+
+            return promocaoDetalhe;
         }
     }
 }
+
