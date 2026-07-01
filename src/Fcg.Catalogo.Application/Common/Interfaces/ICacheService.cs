d49a7a289d484fbac215a958e77d0a263a7b0e96
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-01 00:40:47 -0300

    feat(application): adicionar interface ICacheService para abstracao de cache

---
+++ src/Fcg.Catalogo.Application/Common/Interfaces/ICacheService.cs
@@ -0,0 +1,10 @@
+﻿namespace Fcg.Catalogo.Application.Common.Interfaces
+{
+    public interface ICacheService
+    {
+        Task<T?> ObterAsync<T>(string chaveCache);
+        Task DefinirAsync<T>(string chave, T valor, TimeSpan tempoExpiracao);
+        Task RemoverAsync(string chaveCache);
+        Task RemoverPorPrefixoAsync(string prefixo);
+    }
+}
