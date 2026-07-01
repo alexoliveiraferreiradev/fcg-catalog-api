251fc82f82dec2f0145544044ae07f72f0c3bf13
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-01 12:17:03 -0300

    feat: adicionar Handlers para Domain Events com o intuito de invalidar caches do Redis

---
+++ src/Fcg.Catalogo.Application/Features/Catalogo/EventHandlers/JogoAdicionadoCacheHandler.cs
@@ -0,0 +1,29 @@
+﻿using Fcg.Catalogo.Application.Common.Interfaces;
+using Fcg.Catalogo.Domain.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Fcg.Catalogo.Application.Features.Catalogo.EventHandlers
+{
+    public class JogoAdicionadoCacheHandler : INotificationHandler<JogoAdicionadoEvent>
+    {
+        private readonly ICacheService _cacheService;
+        private readonly ILogger<JogoAdicionadoCacheHandler> _logger;
+
+        public JogoAdicionadoCacheHandler(ILogger<JogoAdicionadoCacheHandler> logger, ICacheService cacheService)
+        {
+            _logger = logger;
+            _cacheService = cacheService;
+        }
+
+        public async Task Handle(JogoAdicionadoEvent notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("[Cache] Iniciando limpeza de cache para o novo JogoId: {JogoId}", notification.JogoId);
+            
+            await _cacheService.RemoveAsync("catalogo:todos");
+            await _cacheService.RemoveByPrefixAsync("catalogo:pag:");
+
+            _logger.LogInformation("[Cache] Cache de listagem de jogos invalidado com sucesso.");
+        }
+    }
+}
---
+++ src/Fcg.Catalogo.Application/Features/Catalogo/EventHandlers/JogoAtualizadoCacheHandler.cs
@@ -0,0 +1,27 @@
+﻿using Fcg.Catalogo.Application.Common.Interfaces;
+using Fcg.Catalogo.Domain.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Fcg.Catalogo.Application.Features.Catalogo.EventHandlers
+{
+    public class JogoAtualizadoCacheHandler : INotificationHandler<JogoAtualizadoEvent>
+    {
+        private readonly ILogger<JogoAtualizadoCacheHandler> _logger;   
+        private readonly ICacheService _cacheService;
+
+        public JogoAtualizadoCacheHandler(ICacheService cacheService, ILogger<JogoAtualizadoCacheHandler> logger)
+        {
+            _cacheService = cacheService;
+            _logger = logger;
+        }
+
+
+        public async Task Handle(JogoAtualizadoEvent notification, CancellationToken cancellationToken)
+        {
+            await _cacheService.RemoveAsync("catalogo:todos");
+            await _cacheService.RemoveAsync($"catalogo:jogo:detalhes:{notification.JogoId}");
+            await _cacheService.RemoveByPrefixAsync("catalogo:pag:");
+        }
+    }
+}
