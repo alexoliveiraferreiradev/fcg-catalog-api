287f5dda598420becb1b8f3450931e43e047cfa5
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-01 00:17:34 -0300

    feat(Infrastructure): Adiciona DesativaPromocaoWorker ao projeto

---
+++ src/Fcg.Catalogo.Infrastructure/Worker/DesativaPromocaoInvalidaWorker.cs
@@ -0,0 +1,27 @@
+﻿using Fcg.Catalogo.Domain.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Fcg.Catalogo.Infrastructure.Worker
+{
+    public class DesativaPromocaoInvalidaWorker : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        public DesativaPromocaoInvalidaWorker(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                using (var scope = _scopeFactory.CreateScope())
+                {
+                    var jogoRepository = scope.ServiceProvider.GetRequiredService<IJogoRepository>();
+                    await jogoRepository.DesativaPromocoesInvalidas();
+                }
+                await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
+            }
+        }
+    }
+}
