a0e17f5a9e82155b18686884fd14bca9ffad3155
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-04 17:46:58 -0300

    refactor(catalog): adiciona DefaultValue às propriedades de AddGameCommand

--- src/Fcg.Catalog.Application/Features/Catalog/Commands/Admin/AddGame/AddGameCommand.cs
+++ src/Fcg.Catalog.Application/Features/Catalog/Commands/Admin/AddGame/AddGameCommand.cs
@@ -1,6 +1,7 @@
 using Fcg.Catalog.Application.Features.Response;
 using Fcg.Catalog.Domain.Enum;
 using MediatR;
+using System.ComponentModel;
 
 namespace Fcg.Catalog.Application.Features.Catalog.Commands.Admin.AddGame
 {
@@ -12,18 +13,22 @@ namespace Fcg.Catalog.Application.Features.Catalog.Commands.Admin.AddGame
         /// <summary>
         /// Nome/Título do jogo.
         /// </summary>
+        [DefaultValue("nome do jogo")]
         public string Name { get; init; } = string.Empty;
         /// <summary>
         /// Descrição detalhada do jogo.
         /// </summary>
+        [DefaultValue("descrição do jogo")]
         public string Description { get; init; } = string.Empty;
         /// <summary>
         /// Preço base do jogo.
         /// </summary>
+        [DefaultValue(59.99)]
         public decimal Price { get; init; }
         /// <summary>
         /// Gênero do jogo.
         /// </summary>
+        [DefaultValue(GameGenre.Action)]
         public GameGenre Genre { get; init; }
     }
 }
