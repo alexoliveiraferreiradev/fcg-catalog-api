d695a057f2a2ac55ffa8ed7a729cda4162a9035d
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-04 01:26:29 -0300

    refactor(domain): padroniza nomenclatura de parametros em ILibraryRepository para camelCase

--- src/Fcg.Catalog.Domain/Repositories/ILibraryRepository.cs
+++ src/Fcg.Catalog.Domain/Repositories/ILibraryRepository.cs
@@ -1,14 +1,14 @@
-﻿using Fcg.Catalog.Domain.Entities;
+using Fcg.Catalog.Domain.Entities;
 
 namespace Fcg.Catalog.Domain.Repositories
 {
     public interface ILibraryRepository
     {
-        void Add(UserLibrary Library);
-        void Update(UserLibrary Library);
+        void Add(UserLibrary library);
+        void Update(UserLibrary library);
         Task<UserLibrary?> GetById(Guid id);
-        Task<bool> CheckIfUserOwnsGame(Guid UserId, Guid GameId);
-        Task<IEnumerable<Guid>> GetPurchasedGamesByUser(Guid UserId);
+        Task<bool> CheckIfUserOwnsGame(Guid userId, Guid gameId);
+        Task<IEnumerable<Guid>> GetPurchasedGamesByUser(Guid userId);
 
     }
 }
