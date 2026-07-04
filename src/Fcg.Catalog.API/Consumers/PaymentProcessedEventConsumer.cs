c467e1e9088816f2ccc42bd2b8c9fd65c3f5a2f8
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-04 15:20:34 -0300

    fix(catalog): corrige termos em ingles no log do PaymentProcessedEventConsumer

--- src/Fcg.Catalog.API/Consumers/PaymentProcessedEventConsumer.cs
+++ src/Fcg.Catalog.API/Consumers/PaymentProcessedEventConsumer.cs
@@ -1,9 +1,4 @@
-﻿using Fcg.Catalog.Application.Features.Orders.Commands.FinalizeOrder;
-using Fcg.Catalog.Domain.Entities;
-using Fcg.Catalog.Domain.Events;
-using Fcg.Catalog.Domain.Repositories;
-using Fcg.Core.Abstractions.Enum;
-using Fcg.Core.Abstractions.Interfaces;
+using Fcg.Catalog.Application.Features.Orders.Commands.FinalizeSucessOrder;
 using Fcg.Core.Abstractions.MessageContracts;
 using MassTransit;
 using MediatR;
@@ -25,8 +20,8 @@ namespace Fcg.Catalog.API.Consumers
         public async Task Consume(ConsumeContext<PaymentProcessedEvent> context)
         {
             var order = context.Message;
-            _logger.LogInformation("[CatalogAPI] PaymentProcessedEvent recebido para Order {OrderId}", order.OrderId);
-            var finalizeCommand = new FinalizeOrderCommand(order.OrderId, order.UserId, order.JogosIds, order.Status);
+            _logger.LogInformation("[CatalogAPI] PaymentProcessedEvent recebido para o Pedido {OrderId}", order.OrderId);
+            var finalizeCommand = new FinalizeSucessOrderCommand(order.OrderId, order.UserId, order.NomeUsuario, order.EmailUsuario, order.JogosIds, order.Status);
             await _mediator.Send(finalizeCommand);
         }
     }
