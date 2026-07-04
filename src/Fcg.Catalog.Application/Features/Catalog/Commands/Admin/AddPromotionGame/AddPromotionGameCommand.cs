87f1e8dfb505eb34536e18e3f9f7e1bc331720d8
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-04 17:47:25 -0300

    refactor(catalog): adiciona DefaultValue às propriedades de AddPromotionGameCommand

--- src/Fcg.Catalog.Application/Features/Catalog/Commands/Admin/AddPromotionGame/AddPromotionGameCommand.cs
+++ src/Fcg.Catalog.Application/Features/Catalog/Commands/Admin/AddPromotionGame/AddPromotionGameCommand.cs
@@ -1,5 +1,6 @@
 using Fcg.Catalog.Application.Features.Response;
 using MediatR;
+using System.ComponentModel;
 
 namespace Fcg.Catalog.Application.Features.Catalog.Commands.Admin.AddPromotionGame
 {
@@ -11,18 +12,22 @@ namespace Fcg.Catalog.Application.Features.Catalog.Commands.Admin.AddPromotionGa
         /// <summary>
         /// Identificador único (GUID) do game ao qual a promoção será associada.
         /// </summary>
+        [DefaultValue("00000000-0000-0000-0000-000000000000")]
         public Guid GameId { get; init; }
         /// <summary>
         /// Preço promocional do game (deve ser menor que o preço original).
         /// </summary>
+        [DefaultValue(29.99)]
         public decimal PromotionValue { get; init; }
         /// <summary>
         /// Data e hora de início da vigência da promoção.
         /// </summary>
+        [DefaultValue("2026-07-04T17:40:00Z")]
         public DateTime StartDate { get; init; }
         /// <summary>
         /// Data e hora de término da vigência da promoção.
         /// </summary>
+        [DefaultValue("2026-07-14T17:40:00Z")]
         public DateTime EndDate { get; init; }
 
         /// <summary>
