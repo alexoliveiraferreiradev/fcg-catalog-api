447bf1ba1263e4803a90ca636cac14dbd5a4ec6f
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-05 13:52:41 -0300

    refactor(application): introduzir interface de consulta ILibraryQueryRepository e simplificar responses

--- src/Fcg.Catalog.Application/Common/Interfaces/IGameQueryRepository.cs
+++ src/Fcg.Catalog.Application/Common/Interfaces/IGameQueryRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Fcg.Catalog.Application.Features.Response;
+using Fcg.Catalog.Domain.Entities;
 using Fcg.Catalog.Domain.Enum;
 using Fcg.Core.Abstractions.Common;
 
@@ -8,6 +9,7 @@ namespace Fcg.Catalog.Application.Common.Interfaces
     {
         Task<PagedResult<GameUserResponse>> GetPagedCatalogAsync(GameGenre? gameGenre,bool? OnlyPromoted ,int pageNumber, int pageSize, CancellationToken cancellationToken);
         Task<GameResponse?> GetGameByIdAsync(Guid gameId, CancellationToken cancellationToken);
-        Task<IEnumerable<GameResponse>> GetAllGamesAsync(CancellationToken cancellationToken);        
+        Task<IEnumerable<GameResponse>> GetAllGamesAsync(CancellationToken cancellationToken);
+        Task<IEnumerable<GameResponse>> GetGamesByIdsAsync(IEnumerable<Guid> jogosIds, CancellationToken cancellationToken);
     }
 }
---
+++ src/Fcg.Catalog.Application/Common/Interfaces/ILibraryQueryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Fcg.Catalog.Application.Features.Library.Queries.GetPagedLibrary;
+using Fcg.Core.Abstractions.Common;
+
+namespace Fcg.Catalog.Application.Common.Interfaces
+{
+    public interface ILibraryQueryRepository
+    {
+        Task<PagedResult<BibliotecaItemResponse>> GetPagedLibraryAsync(Guid userId, int page, int pageSize, CancellationToken cancellationToken);
+        Task<IEnumerable<Guid>> GetPurchasedGamesByUser(Guid userId, CancellationToken cancellationToken);
+    }
+}
