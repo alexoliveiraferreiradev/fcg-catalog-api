d0f8b7efdcab95accf4763c448a0364426e76890
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-04 22:16:53 -0300

    refactor(application/domain): definir interfaces de consulta de cenario CQRS e DTOs de resposta

---
+++ src/Fcg.Catalog.Application/Common/Interfaces/IGameQueryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Fcg.Catalog.Application.Features.Response;
+using Fcg.Catalog.Domain.Enum;
+using Fcg.Core.Abstractions.Common;
+
+namespace Fcg.Catalog.Application.Common.Interfaces
+{
+    public interface IGameQueryRepository
+    {
+        Task<PagedResult<GameUserResponse>> GetPagedCatalogAsync(GameGenre? gameGenre,bool? OnlyPromoted ,int pageNumber, int pageSize, CancellationToken cancellationToken);
+        Task<GameResponse?> GetGameByIdAsync(Guid gameId, CancellationToken cancellationToken);
+        Task<IEnumerable<GameResponse>> GetAllGamesAsync(CancellationToken cancellationToken);        
+    }
+}
