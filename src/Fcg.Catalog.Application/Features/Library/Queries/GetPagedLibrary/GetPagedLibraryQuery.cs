8ea317702d4c8137fedd88714372a0ff2c9798e1
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-24 11:53:18 -0300

    refactor(app): remover features e casos de uso de Order e Library e atualizar dependencias

--- src/Fcg.Catalog.Application/Features/Library/Queries/GetPagedLibrary/GetPagedLibraryQuery.cs
+++
@@ -1,7 +0,0 @@
-﻿using Fcg.Core.Abstractions.Common;
-using MediatR;
-
-namespace Fcg.Catalog.Application.Features.Library.Queries.GetPagedLibrary
-{
-    public record GetPagedLibraryQuery(Guid UserId, int Page =1, int PageSize = 10) : IRequest<PagedResult<BibliotecaItemResponse>>;
-}
--- src/Fcg.Catalog.Application/Features/Orders/Commands/FinalizeSucessOrder/FinalizeSucessOrderCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using Fcg.Core.Abstractions.Enum;
-using MediatR;
-
-namespace Fcg.Catalog.Application.Features.Orders.Commands.FinalizeSucessOrder
-{
-    public record FinalizeSucessOrderCommand(Guid OrderId, Guid UserId, IEnumerable<Guid> GameIds) : IRequest;
-}
