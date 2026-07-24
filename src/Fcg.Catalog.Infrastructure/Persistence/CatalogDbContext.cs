23c755a6cb2bcc4ef66ab981339806d4357866b1
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-24 11:52:58 -0300

    refactor(infra): limpar configuracoes e mapeamentos de Order e Library no Catalogo

--- src/Fcg.Catalog.Infrastructure/Persistence/CatalogDbContext.cs
+++ src/Fcg.Catalog.Infrastructure/Persistence/CatalogDbContext.cs
@@ -8,10 +8,8 @@ namespace Fcg.Catalog.Infrastructure.Persistence
     {
         public CatalogDbContext(DbContextOptions<CatalogDbContext> options) :base(options){            
         }
-        public DbSet<UserLibrary> Libraries  { get; set; }
         public DbSet<Game> Games { get; set; }
         public DbSet<Promotion> Promotions { get; set; }
-        public DbSet<Order> Orders { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogDbContext).Assembly);
