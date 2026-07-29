18dcaedda70a78318c11354a1bd40384c0fc0b34
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-29 17:00:27 -0300

    feat(app): isolar injecao de dependencias em classe de extensao especifica para aplicacao

---
+++ src/Fcg.Catalog.Application/Extensions/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Fcg.Catalog.Application.Features.Catalog.Commands.Admin.AddGame;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fcg.Catalog.Application.Extensions
+{
+    public static class ApplicationServiceCollectionExtensions
+    {     
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(AddGameCommand).Assembly);
+            });
+            services.AddValidatorsFromAssembly(typeof(AddGameCommand).Assembly);
+            return services;
+        }
+    }
+}
