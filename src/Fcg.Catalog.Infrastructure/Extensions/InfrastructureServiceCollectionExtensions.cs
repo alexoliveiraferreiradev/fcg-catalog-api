fc33361d48d0f77e86b5b2509d850d2ea3783810
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-29 17:00:11 -0300

    feat(infra): adicionar suporte a UserSecrets e extensoes de infraestrutura

---
+++ src/Fcg.Catalog.Infrastructure/Extensions/InfrastructureServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Fcg.Catalog.Application.Common.Interfaces;
+using Fcg.Catalog.Domain.Repositories;
+using Fcg.Catalog.Infrastructure.Caching;
+using Fcg.Catalog.Infrastructure.Persistence;
+using Fcg.Catalog.Infrastructure.Queries;
+using Fcg.Catalog.Infrastructure.Queries.DapperHandlers;
+using Fcg.Catalog.Infrastructure.Repositories;
+using Fcg.Catalog.Infrastructure.Worker;
+using Fcg.Core.Abstractions.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+
+namespace Fcg.Catalog.Infrastructure.Extensions
+{
+    public static class InfrastructureServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContextExtension(configuration);
+            services.AddCacheExtension(configuration);
+            services.AddMassTransitExtension(configuration);
+            services.AddScoped<IDbConnection>(sp => sp.GetRequiredService<CatalogDbContext>().Database.GetDbConnection());
+            SqlMapper.AddTypeHandler(new NameTypeHandler());
+            SqlMapper.AddTypeHandler(new DescriptionTypeHandler());
+            SqlMapper.AddTypeHandler(new PriceTypeHandler());
+            services.AddScoped<CatalogDbContext>();
+            services.AddHostedService<DeactivateInvalidPromotionWorker>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ICacheService, RedisCacheService>();
+            services.AddScoped<IGameRepository, GameRepository>();
+            services.AddScoped<IGameQueryRepository, GameQueryRepository>();
+            services.AddScoped<IPromotionQueryRepository, PromotionQueryRepository>();
+            return services;
+        }
+    }
+}
