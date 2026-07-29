8301ce996a402936ba0d0a400b1961dad71000ac
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-29 17:00:44 -0300

    refactor(api): reorganizar extensoes de injecao de dependencias e limpar chaves de infraestrutura dos appsettings

---
+++ src/Fcg.Catalog.API/Extensions/AuthorizationExtensions.cs
@@ -0,0 +1,58 @@
+﻿using Fcg.Core.WebApi.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Fcg.Catalog.API.Extensions
+{
+    public static class AuthorizationExtensions
+    {
+
+        public static WebApplicationBuilder AddAuthorizationExtension(this WebApplicationBuilder builder)
+        {
+            builder.AddPolicyExtension().AddJwtBearerExtension();
+            return builder;
+        }
+        #region Policy
+        private static WebApplicationBuilder AddPolicyExtension(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminOnly", policy => policy.RequireRole("AdminRole"));
+
+                options.AddPolicy("PlayersOnly", policy => policy.RequireRole("PlayerRole"));
+            });
+            return builder;
+        }
+        #endregion
+
+        #region Jwt Bearer
+        private static WebApplicationBuilder AddJwtBearerExtension(this WebApplicationBuilder builder)
+        {
+            var jwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
+            builder.Services.Configure<JwtSettings>(jwtSettingsSection);
+            var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
+            var key = Encoding.ASCII.GetBytes(jwtSettings.Secret);
+            builder.Services.AddAuthentication(opts =>
+            {
+                opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience
+                };
+            });
+            return builder;
+        }
+        #endregion
+    }
+}
